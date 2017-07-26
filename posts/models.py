01c3fe8274023ea1f3b3a28ef2c8fb52756f4b08
alsi3dy <soud94@gmail.com>
2017-07-26 11:00:13 -0500

    Bootsrap and buttons and other cuddly things.

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
+
+	class Meta:
+		ordering = ['title']
---
+++ posts/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+
+
+def post_list(request):
+	obj_list = Post.objects.all()#.order_by("title","updated")#
+	context = {
+		"post_list": obj_list,
+	}
+	return render(request, 'post_list.html', context)
+
+
+def post_detail(request, post_id):
+	obj = get_object_or_404(Post, id=post_id)
+
+	context = {
+		"instance": obj,
+	}
+	return render(request, 'post_detail.html', context)
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "патрясающий!!!")
+		return redirect("posts:list")
+	context = {
+		"form":form,
+	}
+	return render(request, 'post_create.html', context)
+
+
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance=post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "вы готовы написать ?")
+		return redirect("posts:list")
+	context = {
+		"form":form,
+		"post_object":post_object,
+	}
+	return render(request, 'post_update.html', context)
+
+
+def post_delete(request, post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request, "до свидания злой вищь :( ")
+	return redirect("posts:list")
+	
