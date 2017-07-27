5a33e34d6892d165db3484ac7037aeeec93fc415
alsi3dy <soud94@gmail.com>
2017-07-27 12:41:56 -0500

    share and stuff

--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,28 @@ from .models import Post
 from .forms import PostForm
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from urllib.parse import quote
 
 
 def post_list(request):
 	obj_list = Post.objects.all()#.order_by("title","updated")#
+
+	paginator = Paginator(obj_list, 5) 
+
+	page = request.GET.get('page')
+	try:
+		p_objects = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		p_objects = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		p_objects = paginator.page(paginator.num_pages)
+
+	
 	context = {
-		"post_list": obj_list,
+		"post_list": p_objects,
 	}
 	return render(request, 'post_list.html', context)
 
@@ -18,14 +34,15 @@ def post_detail(request, post_id):
 
 	context = {
 		"instance": obj,
+		
 	}
 	return render(request, 'post_detail.html', context)
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		form.save()
-		messages.success(request, "патрясающий!!!")
+		messages.success(request, "yes")
 		return redirect("posts:list")
 	context = {
 		"form":form,
@@ -35,10 +52,10 @@ def post_create(request):
 
 def post_update(request, post_id):
 	post_object = get_object_or_404(Post, id=post_id)
-	form = PostForm(request.POST or None, instance=post_object)
+	form = PostForm(request.POST or None, request.FILES or None, instance=post_object)
 	if form.is_valid():
 		form.save()
-		messages.success(request, "вы готовы написать ?")
+		messages.success(request, "no")
 		return redirect("posts:list")
 	context = {
 		"form":form,
@@ -49,6 +66,6 @@ def post_update(request, post_id):
 
 def post_delete(request, post_id):
 	Post.objects.get(id=post_id).delete()
-	messages.warning(request, "до свидания злой вищь :( ")
+	messages.warning(request, "fdf ")
 	return redirect("posts:list")
 	
