4fa4f0b039a6556617ae8d0418d6751828738ccc
alsi3dy <soud94@gmail.com>
2017-08-01 11:18:21 -0500

    login and signup and other things

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^master/', include('posts.urls', namespace="posts")),
+    url(r'^', include('posts.urls', namespace="posts")),
 ]
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -5,16 +5,83 @@ from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from urllib.parse import quote
+from django.http import Http404
+from django.utils import timezone
+from django.db.models import Q
+from .forms import PostForm, UserSignUp, UserLogin
+from django.contrib.auth import authenticate, login, logout
+
+
+
+def usersignup(request): 
+	context = {}
+	form = UserSignUp()
+	context['form'] = form
+	if request.method == "POST":
+		form = UserSignUp(request.POST)
+		if form.is_valid():
+
+			user = form.save(commit=False)
+			username = user.username
+			password = user.password
+			user.set_password(password)
+			user.save()
+			auth_user = authenticate(username=username, password=password)
+			login(request, auth_user)
+
+			return redirect("posts:list")
+		messages.error(request, forms.errors)
+		return redirect("posts:signup")
+	return render(request,'signup.html', context)
+
+def userlogin(request):
+	context = {}
+	form = UserLogin()
+	context['form'] = form
+	if request.method == "POST":
+		form = UserLogin(request.POST)
+		if form.is_valid():
+			
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			auth_user = authenticate(username=username, password=password)
+			
+			if auth_user is not None:
+				login(request,auth_user)
+				return redirect("posts:list")
+			
+			messages.warning(request, "Wrong username/password combination. Please try again.")
+			return redirect("posts:login")
+		messages.warning(request, form.errors)
+		return redirect("posts:login")
+		
+	return render(request,'login.html', context)
 
 
 def post_list(request):
-	obj_list = Post.objects.all()#.order_by("title","updated")#
+	today= timezone.now().date()
+	if request.user.is_staff or request.user.is_superuser:
+		obj_list = Post.objects.all()
+	else:
+		obj_list = Post.objects.filter(draft=False).filter(publish__lte=today)
+	
+	query= request.GET.get("q")
+	if query:
+		obj_list = obj_list.filter(
+			Q(title__icontains=query)|
+			Q(content__icontains=query)|
+			Q(author__first_name__icontains=query)|
+			Q(author__last_name__icontains=query)|
+			Q(author__username__icontains=query)
+			).distinct() 
 
 	paginator = Paginator(obj_list, 5) 
 
 	page = request.GET.get('page')
 	try:
-		p_objects = paginator.page(page)
+		p_objects = paginator.page(page) 
+		
 	except PageNotAnInteger:
 		# If page is not an integer, deliver first page.
 		p_objects = paginator.page(1)
@@ -25,12 +92,17 @@ def post_list(request):
 	
 	context = {
 		"post_list": p_objects,
+		"today" : today,
 	}
 	return render(request, 'post_list.html', context)
 
 
-def post_detail(request, post_id):
-	obj = get_object_or_404(Post, id=post_id)
+def post_detail(request, slug):
+	obj = get_object_or_404(Post, slug=slug)
+	date = timezone.now().date()
+	if obj.publish > date or obj.draft:
+		if not (request.user.is_staff or request.user.is_superuser):
+			raise Http404
 
 	context = {
 		"instance": obj,
@@ -39,10 +111,14 @@ def post_detail(request, post_id):
 	return render(request, 'post_detail.html', context)
 
 def post_create(request):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404 
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
-		form.save()
-		messages.success(request, "yes")
+		obj = form.save(commit=False)
+		obj.author = request.user
+		obj.save()
+		messages.success(request, "Object created.")
 		return redirect("posts:list")
 	context = {
 		"form":form,
@@ -50,12 +126,14 @@ def post_create(request):
 	return render(request, 'post_create.html', context)
 
 
-def post_update(request, post_id):
-	post_object = get_object_or_404(Post, id=post_id)
+def post_update(request, slug):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404 
+	post_object = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=post_object)
 	if form.is_valid():
 		form.save()
-		messages.success(request, "no")
+		messages.success(request, "Object updated.")
 		return redirect("posts:list")
 	context = {
 		"form":form,
@@ -64,8 +142,14 @@ def post_update(request, post_id):
 	return render(request, 'post_update.html', context)
 
 
-def post_delete(request, post_id):
-	Post.objects.get(id=post_id).delete()
-	messages.warning(request, "fdf ")
+def post_delete(request, slug):
+	if not (request.user.is_superuser):
+		raise Http404 
+	Post.objects.get(slug=slug).delete()
+	messages.warning(request, "Object deleted. ")
 	return redirect("posts:list")
+
+def userlogout(request):
+    logout(request)
+    return redirect("posts:login") 
 	
